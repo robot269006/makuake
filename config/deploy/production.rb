fc7317bb8ad679087ed80226c8cc8122af2761b7
robot269006 <robot269006@yahoo.co.jp>
2017-06-20 23:35:42 +0900

    Create capistrano files
    1. bundle exec cap install
    2. edit Capfile
    3. edit production.rb
    4. edit deploy.rb
    5. edit unicorn.rb

---
+++ config/deploy/production.rb
@@ -0,0 +1,1 @@
+server '52.193.104.1', user: 'ec2-user', roles: %w{app db web}
---
+++ config/deploy/staging.rb
@@ -0,0 +1,23 @@
+lock '3.8.1'
+
+set :application, 'makuake'
+set :repo_url,  'git@github.com:robot269006/makuake.git'
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.3.1'
+
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['/Users/matsuwakajunko/.ssh/makuake.pem']
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
