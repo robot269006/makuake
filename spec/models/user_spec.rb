3da5045bfc7ce5f9ceda57595e4beb05774e2671
robot269006 <robot269006@yahoo.co.jp>
2017-06-28 20:43:39 +0900

    Run rspec for User
    1. test for user creation (expect(@user).to be_valid)
    2. validates_presence_of :user_name
    3. test for user validation(expect(@user).to_not be_valid)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'creation' do
+    before do
+      @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", user_name: "ryota", user_image: "xxxxx.com", user_description: "he's a dude")
+    end
+
+    it 'can create a user' do
+      expect(@user).to be_valid
+    end
+
+    it 'has user validation working' do
+      @user.user_name = nil
+      expect(@user).to_not be_valid
+    end
+  end
 end
