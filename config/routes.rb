209a8eaab3330f713865e614626ffce1a3712d75
robot269006 <robot269006@yahoo.co.jp>
2017-07-03 22:03:37 +0900

    Set project creation function via controller
    1. manually create create.html.haml
    2. set controller -> new and create method
    3. set routing
    4. set view file (new.html.haml)
    5. rspec for the routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # Select from:  index, show, new, edit, create, update, destroy
   # devise_for :users
   root 'projects#index'
-  # resources :projects
+  resources :projects, only: [:index, :new, :create]
   # resources :products, except: [:show]
   # resources :users
 end
--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -9,4 +9,12 @@ RSpec.describe ProjectsController, type: :controller do
     end
   end
 
+  describe "GET #new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+
 end
